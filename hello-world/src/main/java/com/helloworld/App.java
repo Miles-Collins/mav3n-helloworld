096f1f6f8cbefe333370347fd619c9cf3533903b
Miles-Collins <miles.collins@ymail.com>
2026-01-12 14:35:17 -0600

    added world

--- hello-world/src/main/java/com/helloworld/App.java
+++ hello-world/src/main/java/com/helloworld/App.java
@@ -7,6 +7,6 @@ package com.helloworld;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
